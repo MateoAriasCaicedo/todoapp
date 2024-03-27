dcb6203d787164b6e09d6abb551d8d873e540d2e
Luis Mateo Arias Caicedo <mateo.arias@jala.university>
2024-03-27 17:40:34 -0500

    Project setup

---
+++ src/test/java/com/codecrafters/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafters.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
