445bfc7ff57a4e4a64c92f7d9c7d1ccc312a3ec1
Luis Mateo Arias Caicedo <mateo.arias@jala.university>
2024-04-02 11:43:29 -0500

    The repositories had been created

--- src/test/java/com/codecrafters/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/codecrafters/todoapp/TodoappApplicationTests.java
@@ -1,9 +1,7 @@
 package com.codecrafters.todoapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TodoappApplicationTests {
 
 	@Test
